c234920ff36787373e344688a1935dabda07c39c
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2024-10-13 20:08:34 +0900

    変数調整

--- tool/create_signed_path.go
+++ tool/create_signed_path.go
@@ -30,7 +30,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	path := "/rs:fill:300:400/plain/https://storage.googleapis.com/studio-design-asset-files/projects/7kadpxLza3/s-1616x792_v-fms_webp_b5774a47-5fde-4867-987a-3a2bb4664066.webp"
+	transformations := "rs:fill:400:400"
+	imagePath := "https://storage.googleapis.com/studio-design-asset-files/projects/7kadpxLza3/s-1616x792_v-fms_webp_b5774a47-5fde-4867-987a-3a2bb4664066.webp"
+	path := fmt.Sprintf("/%s/plain/%s", transformations, imagePath)
 
 	mac := hmac.New(sha256.New, keyBin)
 	mac.Write(saltBin)
